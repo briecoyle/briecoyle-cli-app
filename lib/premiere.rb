30aca03b2b654a5d75591abeffe49637e095b3a2
Brie Coyle <brie.coyle@gmail.com>
2016-12-14 20:22:58 +0000

    add relationships between premiere and show classes

--- lib/premiere.rb
+++ lib/premiere.rb
@@ -2,5 +2,15 @@ require 'pry'
 require_relative "../lib/show.rb"
 
 class Premiere
+  attr_accessor :date, :shows
+  @@all = []
 
+  def all
+    @@all
+  end
+
+  def add_show(new_show)
+    self.shows << new_show
+    new_show.premiere << self
+  end
 end
--- lib/show.rb
+++ lib/show.rb
@@ -2,5 +2,15 @@ require 'pry'
 require_relative "../lib/premiere.rb"
 
 class Show
+  attr_accessor :premiere
 
+  @@all = []
+  def all
+    @@all
+  end
+
+  def add_premiere(new_premiere)
+    self.all << new_premiere
+    new_premiere.date << self
+  end
 end
