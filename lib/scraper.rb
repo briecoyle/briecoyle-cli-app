a4e8059fb2ee49e3cb92951f2ac743340d35c80b
Brie Coyle <brie.coyle@gmail.com>
2017-01-04 02:33:20 +0000

    Add load_page method to parse initial HTML

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,5 +3,21 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-  # test of name change for GitHub repository. Local and remote repository names changed from briecoyle-cli-app to tivo-premieres-cli-app.
+  def load_page
+    html = open("http://www.metacritic.com/feature/tv-premiere-dates", "User-Agent" => "Brie Coyle")
+    premiere_page = Nokogiri::HTML(html)
+    binding.pry
+  end
+
+  def self.scrape_for_premieres
+    premieres = []
+
+  end
+
+  def self.scrape_for_shows
+    shows = []
+
+  end
 end
+
+Scraper.new.load_page
