aed87f65a036345dbe0aa120a182f16203ded415
Brie Coyle <brie.coyle@gmail.com>
2017-01-08 19:07:34 +0000

    Add method to scrape page for table of information. Add method to create Premiere from table.

--- lib/premiere.rb
+++ lib/premiere.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative "../lib/show.rb"
+require_relative "../lib/scraper.rb"
 
 class Premiere
   attr_accessor :day, :month, :date, :shows
@@ -10,17 +11,27 @@ class Premiere
   end
 
   def self.create_from_scraper(scraper_data)
-    premieres = []
-    premieres << scraper_data.text.split("\r\n")
-    clean_premieres = premieres.flatten!.grep(/([A-Z]{3}\s*\/\s*\w*\s*\d*)/)
-    clean_premieres.each do |premiere_array|
+    scraper_data.css(".sublistbig").each do |premiere|
+      premiere_array = premiere.text.split("\r\n").grep(/([A-Z]{3}\s*\/\s*\w*\s*\d*)/)[0].strip.split(/\s*\/\s*|\s/)
       new_premiere = Premiere.new
-      new_premiere.day = premiere_array[0]
+      new_premiere.day = premiere_array[0].capitalize
       new_premiere.month = premiere_array[1]
       new_premiere.date = premiere_array[2]
+      binding.pry
     end
-    binding.pry
   end
+  # def self.create_from_scraper(scraper_data)
+  #   premieres = []
+  #   premieres << scraper_data.text.split("\r\n")
+  #   clean_premieres = premieres.flatten!.grep(/([A-Z]{3}\s*\/\s*\w*\s*\d*)/)
+  #   clean_premieres.each do |premiere_array|
+  #     new_premiere = Premiere.new
+  #     new_premiere.day = premiere_array[0]
+  #     new_premiere.month = premiere_array[1]
+  #     new_premiere.date = premiere_array[2]
+  #   end
+  #   binding.pry
+  # end
 
   def all
     @@all
@@ -31,3 +42,5 @@ class Premiere
     new_show.premiere << self
   end
 end
+
+Premiere.create_from_scraper(Scraper.new.scrape_page)
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,60 +8,67 @@ class Scraper
     Nokogiri::HTML(html)
   end
 
-  def scrape_for_premieres
+#  def scrape_for_premieres
+#    premiere_page = self.load_page
+#    premiere_page.css(".sublistbig")
+#  end
+
+  def scrape_page
     premiere_page = self.load_page
-    premiere_page.css(".sublistbig")
+    try = premiere_page.css("table")
   end
 
-  def make_premieres
-    premieres = self.scrape_for_premieres
-    premieres.each do |premiere_info|
-      new_premiere = Premiere.create_from_scraper(premiere_info)
-      shows = premiere_info.get_show_info
-      binding.pry
-      shows.each do |shows_info|
-        new_show = Show.create_from_scraper(show_info)
-        new_premiere.add_show(new_show)
-      end
-    end
-  end
+  # def make_premieres
+  #   premieres = self.scrape_for_premieres
+  #   premieres.each do |premiere_info|
+  #     new_premiere = Premiere.create_from_scraper(premiere_info)
+  #     shows = premiere_info.get_show_info
+  #     binding.pry
+  #     shows.each do |show_info|
+  #       new_show = Show.create_from_scraper(show_info)
+  #       new_premiere.add_show(new_show)
+  #     end
+  #   end
+  # end
+#
+#   def scrape_for_shows
+#     shows = []
+#     premiere_page.css(".sublistbig").each do |premiere|
+#       shows << premiere.css(".even")
+#     end
+#     shows
+#   end
+#
+#   def get_show_info
+#     scrape_for_shows.each do |show_info|
+#       show_info.each do |listing|
+#         this_title = listing.css(".title").text.strip
+#         this_genre = listing.css(".title + td").text.strip
+#         this_time = listing.css("td:last-of-type").text.strip
+#
+#         if !this_time.match(/^\d/) && this_time
+#           modified_time = this_time.split(", ")
+#           this_time = modified_time[1]
+#           this_network = modified_time[0]
+#         else
+#           this_network = listing.css("td:last-of-type img").attribute("alt").value
+#         end
+#
+#         shows << {
+#           :title => this_title,
+#           :genre => this_genre,
+#           :time => this_time,
+#           :network => this_network || "Netflix"
+#         }
+#       end
+#     end
+#   end
+#
+#   def make_shows
+#     Show.create_from_scraper(self.get_show_info)
+#   end
+ end
 
-  def scrape_for_shows
-    shows = []
-    premiere_page.css(".sublistbig").each do |premiere|
-      shows << premiere.css(".even")
-    end
-    shows
-  end
-
-  def get_show_info
-    scrape_for_shows.each do |show_info|
-      show_info.each do |listing|
-        this_title = listing.css(".title").text.strip
-        this_genre = listing.css(".title + td").text.strip
-        this_time = listing.css("td:last-of-type").text.strip
-
-        if !this_time.match(/^\d/) && this_time
-          modified_time = this_time.split(", ")
-          this_time = modified_time[1]
-          this_network = modified_time[0]
-        else
-          this_network = listing.css("td:last-of-type img").attribute("alt").value
-        end
-
-        shows << {
-          :title => this_title,
-          :genre => this_genre,
-          :time => this_time,
-          :network => this_network || "Netflix"
-        }
-      end
-    end
-  end
-
-  def make_shows
-    Show.create_from_scraper(self.get_show_info)
-  end
-end
-
-Scraper.new.make_premieres
+Scraper.new.scrape_page
+#premieres = try.css(".sublistbig")
+#shows_per_premiere = try.css(".sublistbig .even")
